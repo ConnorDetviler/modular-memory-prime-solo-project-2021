af6cbfbee5db080702bee69e4299dbc9b8c08b86
Connor Detviler <connor.detviler@gmail.com>
2021-02-09 11:40:23 -0600

    fixed bug where dom would not update upon selecting a patch

--- src/components/PatchView/PatchView.jsx
+++ src/components/PatchView/PatchView.jsx
@@ -1,23 +1,20 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './PatchView.css';
 
 function PatchView() {
 
-    const id = 1 // placeholder - this is the patch's id
-
+    // const id = 1 // placeholder - this is the patch's id
     const dispatch = useDispatch();
 
-    const patchDetails = useSelector((store) => store.details)
 
-    const viewDetails = () => {
-        console.log(patchDetails);
-    }
+    const patchDetails = useSelector((store) => store.details)
+    const patchNames = useSelector((store) => store.names)
 
     useEffect(() => {
         dispatch({
             type: 'FETCH_DETAILS',
-            payload: id
+            payload: 1
         });
 
         dispatch({
@@ -25,8 +22,29 @@ function PatchView() {
         });
     }, []);
 
+    const patchClick = (id) => {
+        dispatch({
+            type: 'FETCH_DETAILS',
+            payload: id
+        });
+    }
+
     return (
         <div>
+            <div>
+                <ul>
+                    {patchNames.map(patch => {
+                        return (
+                            <li
+                                key={patch.id}
+                                onClick={() => patchClick(patch.id)}
+                            >
+                                {patch.title}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
             <h1>{patchDetails.title}</h1>
             <img src={patchDetails.patch_image} />
             <p>{patchDetails.patch_notes}</p>
