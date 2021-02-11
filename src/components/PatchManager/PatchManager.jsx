556e81e948b9457455f865657d5d414763a9664a
Connor Detviler <connor.detviler@gmail.com>
2021-02-11 11:59:11 -0600

    mapped data from reducer to table in PatchManager

--- src/components/PatchManager/PatchManager.jsx
+++ src/components/PatchManager/PatchManager.jsx
@@ -1,12 +1,24 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper
+} from '@material-ui/core'
+
 
 function PatchManager() {
 
     const dispatch = useDispatch();
 
     const userID = useSelector((store) => store.user.id)
+    const allPatches = useSelector((store) => store.allPatches)
 
 
     useEffect(() => {
@@ -15,8 +27,39 @@ function PatchManager() {
             payload: userID
         });
     }, []);
+
+
+
     return (
-        <p>you got to the patch manager</p>
+        <div>
+            {/* <button onClick = {() => console.log(allPatches)} >test</button> */}
+            <TableContainer component={Paper} >
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Tags</TableCell>
+                            <TableCell>Date Created</TableCell>
+                            <TableCell>Last Edited</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {allPatches.map((row) => {
+                            return (
+                                <TableRow key={row.id}>
+                                    <TableCell>{row.title}</TableCell>
+                                    <TableCell>n/a</TableCell>
+                                    <TableCell>{row.date_created}</TableCell>
+                                    <TableCell>n/a</TableCell>
+                                    <TableCell>EDIT</TableCell>
+                                    <TableCell>DELETE</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     )
 }
 
