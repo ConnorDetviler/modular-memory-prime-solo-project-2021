c0ba23e5ecec02560c8a07bae7aa963cf5b88ae2
Connor Detviler <connor.detviler@gmail.com>
2021-05-05 16:06:37 -0500

    changed all-patches.router to be a SQL transaction

---
+++ server/routes/all-patches.router copy.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
+/**
+ * GET route for all data of all of current user's patches
+ */
+router.get('/', rejectUnauthenticated, (req, res) => {
+
+    let id = req.user.id;
+    console.log('in all-patches.router - user id:', id)
+
+    const queryText = `
+        SELECT * FROM "patch"
+        WHERE "patch".user_id = $1;
+    `;
+
+    pool
+    .query(queryText, [id])
+    .then((result) => {
+        const patchesArr = result.rows;
+
+        // second query for getting all associated tags for each patch
+        const tagQueryText = `
+            SELECT "tag".id, "tag".name FROM "patch_tag"
+            JOIN "tag" ON "patch_tag".tag_id = "tag".id
+            WHERE "patch_id" = $1;
+        `;
+
+        function sendData(dataToSend) {
+            // console.log(dataToSend)
+            res.send(dataToSend)
+        }
+        function getTags(i) {
+            pool
+            .query(tagQueryText, [patchesArr[i].id])
+            .then((result) => {
+                patchesArr[i].tags = result.rows
+    
+                // when loop is finished:
+                if (i === patchesArr.length - 1) {
+                    sendData(patchesArr)
+                }
+
+            })
+            .catch((err) => {
+                console.log(`Error completing SELECT in tag query within all-patches`, err)
+            })
+        }
+
+        // does a query for each patch in the result.rows from last query to get it's tags
+
+            for (let i = 0; i < patchesArr.length; i++) {
+                ctagsFromServer = getTags(i)
+            }
+        }
+    )
+    .catch((err) => {
+        console.error("Error completing SELECT all patches query", err);
+        res.sendStatus(500)
+    })
+});
+
+module.exports = router;
--- server/routes/all-patches.router.js
+++ server/routes/all-patches.router.js
@@ -6,60 +6,39 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 /**
  * GET route for all data of all of current user's patches
  */
-router.get('/', rejectUnauthenticated, (req, res) => {
+router.get('/', rejectUnauthenticated, async (req, res) => {
+    const connection = await pool.connect();
+    try {
+        connection.query('BEGIN;');
 
-    let id = req.user.id;
-    console.log('in all-patches.router - user id:', id)
+        const patchQuery = `
+            SELECT * FROM "patch"
+            WHERE "patch".user_id = $1;
+        `;
 
-    const queryText = `
-        SELECT * FROM "patch"
-        WHERE "patch".user_id = $1;
-    `;
-
-    pool
-    .query(queryText, [id])
-    .then((result) => {
-        const patchesArr = result.rows;
-
-        // second query for getting all associated tags for each patch
-        const tagQueryText = `
+        const tagQuery = `
             SELECT "tag".id, "tag".name FROM "patch_tag"
             JOIN "tag" ON "patch_tag".tag_id = "tag".id
             WHERE "patch_id" = $1;
         `;
 
-        function sendData(dataToSend) {
-            // console.log(dataToSend)
-            res.send(dataToSend)
-        }
-        function getTags(i) {
-            pool
-            .query(tagQueryText, [patchesArr[i].id])
-            .then((result) => {
-                patchesArr[i].tags = result.rows
-    
-                // when loop is finished:
-                if (i === patchesArr.length - 1) {
-                    sendData(patchesArr)
-                }
+        const patches = (await connection.query(patchQuery, [req.user.id])).rows;
 
-            })
-            .catch((err) => {
-                console.log(`Error completing SELECT in tag query within all-patches`, err)
-            })
+        for (let i = 0; i < patches.length; i++) {
+            patches[i].tags = (await connection.query(tagQuery, [patches[i].id])).rows;
         }
+        console.log(patches);
 
-        // does a query for each patch in the result.rows from last query to get it's tags
+        connection.query('COMMIT;');
+        res.send(patches)
 
-            for (let i = 0; i < patchesArr.length; i++) {
-                ctagsFromServer = getTags(i)
-            }
-        }
-    )
-    .catch((err) => {
-        console.error("Error completing SELECT all patches query", err);
-        res.sendStatus(500)
-    })
-});
+    } catch (err) {
+        console.log('Error in GET transaction in all-patches.router. rollback: ', err)
+        await connection.query('ROLLBACK;');
+        res.sendStatus(500);
+    } finally {
+        connection.release();
+    }
+})
 
 module.exports = router;
